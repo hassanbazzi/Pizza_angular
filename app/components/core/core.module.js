73b518a3d97838bab1391c5361d5b50be102ea82
Marko Mazibrada <Marko Mazibrada>
2016-04-23 22:34:56 +0200

    Added basic routing and home page

--- app/components/core/core.module.js
+++ app/components/core/core.module.js
@@ -1,3 +1,3 @@
 (function () {
-    angular.module('pizzaModule.core', ["ui-router"]);
+   angular.module('pizzaModule.core', ["ui.router"]);
 })();
---
+++ app/components/core/core.router.js
@@ -0,0 +1,28 @@
+(function () {
+   angular
+      .module('pizzaModule.core')
+      .config(config);
+
+   config.$inject = ["$stateProvider", "$urlRouterProvider"];
+   function config ($stateProvider,$urlRouterProvider) {
+      $urlRouterProvider.otherwise("/home");
+
+      $stateProvider
+         .state("main",{
+            abstract: true,
+            views: {
+               "navbar": {
+                  templateUrl: "/app/components/core/navbar.html"
+               }
+            }
+         })
+         .state("main.home",{
+            url: "/home",
+            views: {
+               "content@": {
+                  templateUrl: "/app/components/core/home.html"
+               }
+            }
+         });
+  }
+})();
